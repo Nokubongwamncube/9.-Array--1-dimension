072ec7da5d66b76c073f8b03c24e2d14beb0d4e6
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-13 03:23:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Array__1_dimension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckynumbers = { 1, 2, 5, 9, 7, 8, 10, 11, 12 };
+
+            Console.WriteLine(luckynumbers.Length);
+            Console.WriteLine(luckynumbers[0]);
+
+            string[] friends = new string[5];
+            friends[0] = "Nozi";
+            friends[1] = "Simo";
+            friends[2] = "Fiso";
+
+            Console.WriteLine(friends[2]);
+
+
+
+            //frezzing console
+            Console.ReadLine();
+
+                
+        }
+    }
+}
